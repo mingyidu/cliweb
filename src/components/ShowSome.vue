4fadbfdc70bfe346ba2c5170acbde1ebfc895fc8
mingyidu <myvipdu@163.com>
2019-07-07 00:57:13 +0800

    增加show some组件

---
+++ src/components/ShowSome.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1 @click="say_hello">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowSome',
+  data () {
+    return {
+      msg: 'Show something for YOU!'
+    }
+  },
+  methods: {
+	  say_hello () {
+		  alert('hello us')
+	  }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.hello {
+  text-align: center;
+}
+</style>
