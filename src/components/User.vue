c7c5371efe0290f598b5309c5d0473fc027d669f
mingyidu <myvipdu@163.com>
2019-07-07 01:41:23 +0800

    新增用户组件

---
+++ src/components/User.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="user">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'User list',
+  data () {
+    return {
+      msg: 'User list'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.user {
+	text-align: center;
+}
+</style>
